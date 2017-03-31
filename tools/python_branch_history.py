ff104651874f0c56437053da6d242eb1e456725c
zhangsheng <zhangsheng_01@juran.com.cn>
2017-03-31 09:22:54 +0800

    Add several functions: 1. Add a new colume to display relese or not 2. Add an argument of specific datetime to control relese or not 3. Print all projects in console to ensure there's no missing project

--- tools/python_branch_history.py
+++ tools/python_branch_history.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python
+# usage example: python python_branch_history.py "2017 3 22 23 40"
+#
 import base64
-import time
+import datetime
 import os
 import gitlab
+import sys
 
 def main():
     # token authentication from config file
@@ -10,12 +14,14 @@ def main():
     # projects 
     # Note: difference among gl.projects.list() / gl.projects.list(all=True) / gl.projects.owned() 
     projects = gl.projects.list(all=True)
-
+    print('all projects count: ' + str(len(projects)))
+    print('all projects name - ')
     project_release_branches = []
 
     for project in projects:
         # use try/except to avoid script exit from exception when there's no 'release' branch
         try:
+            print('  '+ project.name)
             project_release_branch = project.branches.get('release')
         except Exception:
             project_release_branch = None
@@ -35,21 +41,42 @@ def export(branches):
     script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
     rel_path = "release_branches.md"
     abs_file_path = os.path.join(script_dir, rel_path)
+    temp_argv_list = sys.argv[1].split()
+    year   = int(temp_argv_list[0])
+    month  = int(temp_argv_list[1])
+    day    = int(temp_argv_list[2])
+    hour   = int(temp_argv_list[3])
+    minute = int(temp_argv_list[4])
+    valid_date = unicode(datetime.datetime(year,month,day,hour,minute))
+
+    print('\n')
+    print('projects (have release branch) - ')
+
     with open(abs_file_path, mode="w") as out_file:
-        out_file.write("|Project name|release branch latest modified time|\n")
-        out_file.write("|---|---|\n")
+        out_file.write("|Project name|release branch latest modified time|Release or not(Yes/No)|\n")
+        out_file.write("|---|---|---|\n")
         for branch in branches:
             project_name = branch['name']
             latest_modified = branch['latest_modified']
+            release_or_not = 'Yes'
 
-            print(' project name:  ' + project_name + ' | ' + 'release branch latest modified time: ' + latest_modified)
+            if latest_modified < valid_date:
+                release_or_not = 'No'
+
+            print('  project name:  ' + project_name + ' | ' + 'release branch latest modified time: ' + latest_modified + ' | ' + 'Release or not(Yes/No) : ' + release_or_not )
 
             out_file.write("|")
             out_file.write(project_name)
             out_file.write("|")
             out_file.write(latest_modified)
             out_file.write("|")
+            out_file.write(release_or_not)
+            out_file.write("|")
             out_file.write("|\n")
 
 if __name__ == "__main__":
-    main()
+    if (len(sys.argv) == 2):
+        main()
+    else:
+        print('wrong arguments number(2 arguments)!')
+    
